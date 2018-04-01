6875222a05bac86fbd45e0d6a56255055df207a4
Erik Pearson <eapearson@lbl.gov>
2018-04-01 13:45:06 -0700

    viewModelBase integrates nanobus

--- src/lib/viewModelBase.js
+++ src/lib/viewModelBase.js
@@ -1,27 +1,51 @@
 define([
     'knockout',
-    './subscriptionManager'
+    './subscriptionManager',
+    './nanoBus'
 ], function (
     ko,
-    SubscriptionManager
+    SubscriptionManager,
+    NanoBus
 ) {
 
     class ViewModelBase {
-        constructor() {
+        constructor(params) {
             this.subscriptions = new SubscriptionManager();
             // is this kosher? Maybe a better way of tying in 
             // functionality to the current knockout system?
             // E.g. passing in a ko in the constructor?
             this.observable = ko.observable;
             this.observableArray = ko.observableArray;
+            this.bus = new NanoBus();
+
+            this.parentBus = params.bus;
         }
 
         subscribe(observable, fun) {
             this.subscriptions.add(observable.subscribe(fun));
         }
 
+        sendToParent(message, payload) {
+            if (this.parentBus) {
+                this.parentBus.send(message, payload);
+            }
+        }
+
+        receiveFromParent(message, handler) {
+            this.parentBus.on(message, handler);
+        }
+
+        send(message, payload) {
+            this.bus.send(message, payload);
+        }
+
+        on(message, handler) {
+            this.bus.on(message, handler);
+        }
+
         dispose() {
             this.subscriptions.dispose();
+            this.bus.stop();
         }
     }
 
